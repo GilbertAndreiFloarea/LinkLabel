cbecd5e3da4f7a2e05b629013fa2ceed0ec48758
Gilbert Andrei Floarea <gilbertandrei@gmail.com>
2022-01-21 22:10:47 +0200

    added LinkLabel to the project

---
+++ LinkLabel/LinkLabel/LinkLabel.h
@@ -0,0 +1,18 @@
+//
+//  LinkLabel.h
+//  LinkLabel
+//
+//  Created by Gilbert Andrei Floarea on 21.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LinkLabel.
+FOUNDATION_EXPORT double LinkLabelVersionNumber;
+
+//! Project version string for LinkLabel.
+FOUNDATION_EXPORT const unsigned char LinkLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkLabel/PublicHeader.h>
+
+
